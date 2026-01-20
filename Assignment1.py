fbc597cefbfffb932c974b75376e73efe368680f
payalhpatil <payalhpatil2007@gmail.com>
2026-01-20 13:15:09 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,30 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #TASK 1
+>>> 
+>>> num1=input("Enter the first number: ")
+Enter the first number: 5
+>>> num2=input("Enter the second number:")
+Enter the second number:10
+>>> add=int(num1)+int(num2)
+>>> sub=int(num1)-int(num2)
+>>> mul=int(num1)*int(num2)
+>>> div=int(num1)/int(num2)
+>>> 
+>>> print("Addition: ",add)
+Addition:  15
+>>> print("Subtraction: ",sub)
+Subtraction:  -5
+>>> print("Multiplication: ",mul)
+Multiplication:  50
+>>> print("Division; ",div)
+Division;  0.5
+>>> 
+>>> #TASK 2
+>>> 
+>>> first_name=input("Enter your first name: ")
+Enter your first name: John
+>>> last_name=input("Enter your last name: ")
+Enter your last name: Doe
+>>> print("Hello, ",first_name," ",last_name,"! Welcome to the Python program.")
+Hello,  John   Doe ! Welcome to the Python program.
