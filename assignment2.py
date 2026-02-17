5ef1edaf97e4077e49ee9d99ebdc36780cdc1ebf
payalhpatil <payalhpatil2007@gmail.com>
2026-02-17 20:28:02 +0530

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -1,13 +1,17 @@
-# TASK 1
-num=int(input("Enter a number:"))
-if num%2==0:
-    print(f" {num} is even number")
-else:
-    print(f"{num} is odd number")
+#TASK 1
 
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print(f"The number {number} is Even.")
+else:
+    print(f"The number {number} is Odd.")
 
 #TASK 2
+    
 sum=0
 for i in range(1,51):
     sum=sum+i
-print(f"Sum of numbers from 1 to 50 is {sum))    
+print(f"The sum of numbers from 1 to 50 is: {sum} ")
+  
+
